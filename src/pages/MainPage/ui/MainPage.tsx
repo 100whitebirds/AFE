ee555da38a811084e772018a094d56548373609f
100whitebirds <avenlardo@gmail.com>
2023-07-04 10:18:03 +0600

    stories WIP

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,1 +1,12 @@
-export const MainPage = () => <div>MainPage</div>;
+import { useEffect } from 'react';
+
+export const MainPage = () => {
+  return (
+    <div id="parent" onClick={() => console.log('Parent clicked')}>
+      PARENT
+      <div id="child" onClick={() => console.log('Child clicked')}>
+        CHILD
+      </div>
+    </div>
+  );
+};
--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,13 +1,13 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTheme } from 'app/providers/theme';
 import { Theme } from 'app/providers/theme/lib/ThemeContext';
 import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
-    className?: string;
+  className?: string;
 }
 
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
@@ -15,7 +15,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 
   return (
     <Button
-      theme={ThemeButton.CLEAR}
+      theme={ButtonTheme.CLEAR}
       className={classNames(cls.ThemeSwitcher, {}, [className])}
       onClick={toggleTheme}
     >
