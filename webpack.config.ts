22a4272d52e07abb6c22c4f1d400f461edea06ee
100whitebirds <avenlardo@gmail.com>
2023-07-15 14:28:01 +0600

    troubleshooting workflow

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import { BuildEnv, BuildPaths } from './config/build/types';
-import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { buildWebpackConfig } from './config/build/buildWebpackConfi';
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
