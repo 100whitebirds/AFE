f5952035f130ffd2d55dafab279b600cdec9d112
100whitebirds <avenlardo@gmail.com>
2023-07-16 23:22:08 +0600

    ci troubleshooting

---
+++ src/shared/config/i18n/en.ts
@@ -0,0 +1,10 @@
+export const en = {
+  main: 'Main',
+  about: 'About',
+  lang: 'English',
+  short_lang: 'En',
+  unexpected_error: 'Unexpected error occured',
+  refresh_page: 'Refresh page',
+  page_not_found: 'Page not found',
+  loading: 'Loading',
+};
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import webpack from 'webpack';
 import path from 'path';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
@@ -12,14 +12,15 @@ export default (env: BuildEnv) => {
   };
 
   const mode = env.mode || 'development';
+  const PORT = env.port || 3000;
+
   const isDev = mode === 'development';
-  const port = env.port || 3001;
 
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
     paths,
     isDev,
-    port,
+    port: PORT,
   });
 
   return config;
