315edb274a49225e570a731aac711c1b53569f85
100whitebirds <avenlardo@gmail.com>
2023-07-17 00:06:17 +0600

    ci troubleshooting

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -1,20 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import { Language, resources } from './i18n';
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    lng: Language.EN,
-    fallbackLng: Language.EN,
-    debug: false,
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  debug: false,
 
-    interpolation: {
-      escapeValue: false,
-    },
-    resources,
-  });
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: { ru: { translations: {} } },
+});
 
 export default i18n;
