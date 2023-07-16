85aa09998627c1ad07c0280ee81f42966ad7030b
100whitebirds <avenlardo@gmail.com>
2023-07-16 23:14:06 +0600

    ci troubleshooting

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,14 +12,15 @@ export default (env: BuildEnv) => {
   };
 
   const mode = env.mode || 'development';
+  const PORT = env.port || 3000;
+
   const isDev = mode === 'development';
-  const port = env.port || 3001;
 
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
     paths,
     isDev,
-    port,
+    port: PORT,
   });
 
   return config;
