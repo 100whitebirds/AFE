274bcc558a32ace344303d573f54a0e859d91fa8
100whitebirds <avenlardo@gmail.com>
2023-07-17 00:20:21 +0600

    ci troubleshooting

--- src/shared/lib/tests/componentRender.tsx
+++ src/shared/lib/tests/componentRender.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
-import i18nForTests from 'shared/config/i18n/i18nForTests';
+import i18n from 'shared/config/i18n/i18n';
 import { MemoryRouter } from 'react-router-dom';
 
 // import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
@@ -21,7 +21,7 @@ export function componentRender(
   return render(
     <MemoryRouter initialEntries={[route]}>
       {/* <StoreProvider initialState={initialState}> */}
-      <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+      <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
       {/* </StoreProvider> */}
     </MemoryRouter>,
   );
