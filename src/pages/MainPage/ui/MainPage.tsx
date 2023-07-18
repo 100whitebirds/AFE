af7844e88f4598831182d92f28a0bb62530393ca
100whitebirds <avenlardo@gmail.com>
2023-07-19 01:49:29 +0600

    add pre commit

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,3 @@
-import { Counter } from 'entities/Counter';
-import { useState } from 'react';
-import { Input } from 'shared/ui/Input/Input';
-
 export const MainPage = () => {
-  const [value, setValue] = useState('');
-
-  return (
-    <div>
-      <Input
-        placeholder="Введите текст"
-        onChange={setValue}
-      />
-    </div>
-  );
+  return <div />;
 };
