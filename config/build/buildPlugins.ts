3a6dbe91d025c6b0d4fc7405255c98bff1dda602
100whitebirds <avenlardo@gmail.com>
2023-08-13 12:01:25 +0600

    add statoscope bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import StatoscopeWebpackPlugin  from '@statoscope/webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
@@ -30,6 +31,9 @@ export function buildPlugins({
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin());
     plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new StatoscopeWebpackPlugin({
+      open: 'file',
+    }));
     plugins.push(
       new BundleAnalyzerPlugin({
         openAnalyzer: false,
